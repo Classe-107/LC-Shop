4179fc6373166d903f257c0ba7c1f1ee302987f4
klelia <clelia.fradella@gmail.com>
2023-12-13 16:21:07 +0100

    first commit

---
+++ books.php
@@ -0,0 +1,15 @@
+<?php
+include __DIR__ . '/Views/header.php';
+include __DIR__ . '/Model/Book.php';
+
+?>
+<section class="container">
+    <h2>Book</h2>
+    <div class="row gy-4">
+
+
+    </div>
+</section>
+<?php
+include __DIR__ . '/Views/footer.php';
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__ . '/Views/header.php';
+include __DIR__ . '/Model/Movie.php';
+
+?>
+<section class="container">
+    <h2>Movies</h2>
+    <div class="row gy-4">
+
+
+
+    </div>
+</section>
+<?php
+include __DIR__ . '/Views/footer.php';
+?>
